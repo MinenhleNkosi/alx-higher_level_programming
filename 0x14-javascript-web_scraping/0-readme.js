e4a0f69b65b5ee764bbd2eb8a23cbbbbba80ccc2
jetrocuba101 <minenclenkosi@gmail.com>
2023-07-27 03:53:50 +0200

    Web Frdamework Updates

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+let fs = require('fs');
+let file = process.argv[2];
+
+function readPrint (file) {
+  fs.readFile(file, 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+readPrint(file);
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
-});
+const url = process.argv[2];
+
+function statusCodes (url) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('code: ' + response.statusCode);
+    }
+  });
+}
+statusCodes(url);
